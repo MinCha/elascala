225a6315558e4599a9105a292168ae9680e016db
Min Cha <minslovey@gmail.com>
2015-02-21 02:33:29 +0900

    add http method logging.

--- src/main/scala/elascala/HttpClient.scala
+++ src/main/scala/elascala/HttpClient.scala
@@ -1,10 +1,11 @@
 package elascala
 
+import elascala.HttpMethod.HttpMethod
 import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 import scalaj.http.{Http, HttpResponse}
-
+import HttpMethod._
 /**
  * Created by Vayne on 2015-02-18.
  */
@@ -12,20 +13,9 @@ object HttpClient {
   val logger = LoggerFactory.getLogger(classOf[Elascala])
 
   def get(url: String): HttpResult = {
-    execute(url, "") {
+    execute(Get, url, "") {
       http(url).asString
     }
-  }
-
-  private def http(url: String) = {
-    Http(url).timeout(5000, 5000)
-  }
-
-  private def execute(url: Any, body: Any)(f: => HttpResponse[String]): HttpResult = {
-    val result = f
-    logger.info("url: {}, body: {} => status: {}",
-      List(url, body, result.code).asJava.toArray: _*)
-    HttpResult(result.code, result.body)
   }
 
   def put(url: String, any: Any): HttpResult = {
@@ -33,14 +23,26 @@ object HttpClient {
   }
 
   def put(url: String, body: String): HttpResult = {
-    execute(url, body) {
+    execute(Put, url, body) {
       http(url).method("put").postData(body).asString
     }
   }
 
   def post(url: String, body: String): HttpResult = {
-    execute(url, body) {
+    execute(Post, url, body) {
       http(url).method("post").postData(body).asString
     }
   }
+
+  private def http(url: String) = {
+    Http(url).timeout(5000, 5000)
+  }
+
+  private def execute(method: HttpMethod, url: Any, body: Any)(f: => HttpResponse[String]): HttpResult = {
+    val result = f
+    logger.info("url: {} {}, body: {} => status: {}",
+      List(method.toString.toUpperCase, url, body, result.code).asJava.toArray: _*)
+    HttpResult(result.code, result.body)
+  }
 }
+
---
+++ src/main/scala/elascala/HttpMethod.scala
@@ -0,0 +1,9 @@
+package elascala
+
+/**
+ * Created by Vayne on 2015-02-21.
+ */
+object HttpMethod extends Enumeration {
+  type HttpMethod = Value
+  val Get, Put, Post, Head = Value
+}
